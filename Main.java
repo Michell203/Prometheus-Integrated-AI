99d5f8b9b44f4dbb3c86deb41c8e694aff883942
Michel Hijazin <81302943+Michell203@users.noreply.github.com>
2023-05-05 11:24:46 -0400

    Add files via upload
    Solved XOR problem with perceptrons

--- Main.java
+++ Main.java
@@ -4,9 +4,8 @@ import org.neuroph.core.data.DataSetRow;
 import org.neuroph.nnet.Perceptron;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello World!");
 
+    static void train_OR(){
         //Perceptron network
         NeuralNetwork neuralNetwork = new Perceptron(2, 1);
 
@@ -27,6 +26,38 @@ public class Main {
         trainingSet.add(new DataSetRow(input3, output3));
 
         double[] input4 = new double[]{1,1};
+        double[] output4 = new double[]{1};
+        trainingSet.add(new DataSetRow(input4, output4));
+
+        //Learn training set
+        neuralNetwork.learn(trainingSet);
+
+        //Save trained network to file
+        neuralNetwork.save("or_perceptron.nnet");
+
+    }
+
+    static void train_NAND(){
+        //Perceptron network
+        NeuralNetwork neuralNetwork = new Perceptron(2, 1);
+
+        //Training set
+        DataSet trainingSet = new DataSet(2,1);
+
+        //Training data to training set (Logical XOR)
+        double[] input1 = new double[]{0,0};
+        double[] output1 = new double[]{1};
+        trainingSet.add(new DataSetRow(input1, output1));
+
+        double[] input2 = new double[]{0,1};
+        double[] output2 = new double[]{1};
+        trainingSet.add(new DataSetRow(input2, output2));
+
+        double[] input3 = new double[]{1,0};
+        double[] output3 = new double[]{1};
+        trainingSet.add(new DataSetRow(input3, output3));
+
+        double[] input4 = new double[]{1,1};
         double[] output4 = new double[]{0};
         trainingSet.add(new DataSetRow(input4, output4));
 
@@ -34,20 +65,77 @@ public class Main {
         neuralNetwork.learn(trainingSet);
 
         //Save trained network to file
-        neuralNetwork.save("xor_perceptron.nnet");
+        neuralNetwork.save("nand_perceptron.nnet");
 
+    }
+
+    static void train_AND(){
+        //Perceptron network
+        NeuralNetwork neuralNetwork = new Perceptron(2, 1);
+
+        //Training set
+        DataSet trainingSet = new DataSet(2,1);
+
+        //Training data to training set (Logical XOR)
+        double[] input1 = new double[]{0,0};
+        double[] output1 = new double[]{0};
+        trainingSet.add(new DataSetRow(input1, output1));
+
+        double[] input2 = new double[]{0,1};
+        double[] output2 = new double[]{0};
+        trainingSet.add(new DataSetRow(input2, output2));
+
+        double[] input3 = new double[]{1,0};
+        double[] output3 = new double[]{0};
+        trainingSet.add(new DataSetRow(input3, output3));
+
+        double[] input4 = new double[]{1,1};
+        double[] output4 = new double[]{1};
+        trainingSet.add(new DataSetRow(input4, output4));
+
+        //Learn training set
+        neuralNetwork.learn(trainingSet);
+
+        //Save trained network to file
+        neuralNetwork.save("and_perceptron.nnet");
+
+    }
+
+    static double xor(int inp1, int inp2){
         //Load saved network
-//        NeuralNetwork neuralNetworkLoad = NeuralNetwork.createFromFile("xor_perceptron.nnet");
+        train_OR();
+        train_NAND();
+        train_AND();
 
-//        //Give inputs
-//        neuralNetworkLoad.setInput(1,0);
+        NeuralNetwork orGate = NeuralNetwork.createFromFile("or_perceptron.nnet");
+        NeuralNetwork nandGate = NeuralNetwork.createFromFile("nand_perceptron.nnet");
+        NeuralNetwork andGate = NeuralNetwork.createFromFile("and_perceptron.nnet");
 
-//        //Calculate output
-//        neuralNetworkLoad.calculate();
+        //Give inputs
+        orGate.setInput(inp1,inp2);
+        nandGate.setInput(inp1,inp2);
 
-//        //Get output
-//        double[] output = neuralNetworkLoad.getOutput();
-//        System.out.print(output);
+        //Calculate and get output
+        orGate.calculate();
+        double[] orOut = orGate.getOutput();
+
+        nandGate.calculate();
+        double[] nandOut = nandGate.getOutput();
+
+        //Pass through and
+        andGate.setInput(orOut[0],nandOut[0]);
+        andGate.calculate();
+        double[] xorVal = andGate.getOutput();
+
+        return xorVal[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+
+        //Get output
+        double output = xor(1,0);
+        System.out.print(output);
 
     }
 }
